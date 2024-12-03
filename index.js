cb5c2d4f78ee9246b870987f001ac449185c8b93
Vaibhav Roman <vaibhavroman249@gmail.com>
2024-12-03 12:20:36 +0530

    EJS 22

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express=require("express");
 const urlRoute=require("./routes/url");
+const path=require("path");
 const {connectToMongoDB}=require("./connect")
+const staticRoute=require("./routes/staticRouter")
 const URL=require("./models/url")
 const app=express();
 const PORT=8001;
@@ -9,15 +11,31 @@ const PORT=8001;
 //MongoDB connection with database
 connectToMongoDB("mongodb://127.0.0.1:27017/short-url").then(()=>console.log("MongoDB connected"));
 
-//middleware
+//Set the view engine
+app.set("view engine","ejs");
+//Tell the path of views folder
+app.set("views",path.resolve("./views"));
+//middleware which support JSON data
 app.use(express.json());
+//middleware which support the FORM data
+app.use(express.urlencoded({extended:false}));
+
+
+// app.get("/test", async(req,res)=>{
+//     const allUrls=await URL.find({});
+//     //Used to render our home page in views possible due to EJS
+//     return res.render("home",{
+//         urls:allUrls,
+//     });
+// });
 
 //Routes
 app.use("/url",urlRoute);
+app.use("/",staticRoute);
 
 
 //We are redirecting the shortId to the redirectURL(Original url)
-app.get("/:shortId",async(req,res)=>{
+app.get("/url/:shortId",async(req,res)=>{
     const shortId=req.params.shortId;
     console.log(shortId);
     const entry=await URL.findOneAndUpdate({
